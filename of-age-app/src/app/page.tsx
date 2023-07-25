76c21553bca4a364b19a7c95fe2394d8c6c5866f
daja-w <104468175+daja-w@users.noreply.github.com>
2023-07-24 18:42:34 -0600

    pre mobile first

--- of-age-app/src/app/page.tsx
+++ of-age-app/src/app/page.tsx
@@ -8,89 +8,77 @@ import profile3 from './profile-3.jpg'
 
 export default function Home() {
   return (
-    <main className='px-12 text-black'>
-      <section>
-        <div style={{backgroundColor: "yellow"}} className='p-8'>
-          <hr className='mb-5' style={{borderColor: "black"}}></hr>
-          <div className='grid grid-cols-2 gap-8'>
-            <div className='col-span-1 '>
-              <div className='flex flex-col justify-between h-full'>
-                <div className='pt-14'>
-                  <h1 style={{fontSize: 96, lineHeight: 1.05}}>Your first digital art collection.</h1>
-                  <hr className='mt-5'style={{borderColor: "black"}}></hr>
-                  <div className='grid grid-cols-3'>                       
-                    <div className='col-span-1 flex -space-x-2 overflow-hidden mt-5'>
-                      <Image
-                        className="inline-block h-10 w-10 rounded-full"
-                        src={profile1}
-                        alt=""
-                        style={{objectFit: "cover"}}
-                      />
-                      <Image
-                        className="inline-block h-10 w-10 rounded-full"
-                        src={profile3}
-                        alt=""
-                        style={{objectFit: "cover"}}
-                      />
-                      <Image
-                        className="inline-block h-10 w-10 rounded-full"
-                        src={profile2}
-                        alt=""
-                        style={{objectFit: "cover"}}
-                      />
-                    </div>
-                    <div className='col-span-2'>
-                      <h2 className='mt-5'>JOIN OUR 150K+ COMMUNITY OF TALENTED ARTISTS</h2>
-                    </div>
+    <main className='text-black overflow-hidden'>
+      <section style={{backgroundColor: "yellow"}}>
+        <nav className="hidden md:flex items-center justify-between px-8 pt-4">
+          <div className="flex space-x-4">
+            <a href="#" >About</a>
+            <a href="#" >How it works</a>
+            <a href="#" >Pricing</a>
+            <a href="#" >FAQ</a>
+          </div>
+          <div>
+            <Image src="/logo.jpg" alt="Logo" width={80} height={80} />
+          </div>
+          <div className="flex space-x-4 items-center">
+            <a href="#" >Blog</a>
+            <a href="#" >Contacts</a>
+            <button className="rounded-full butt">Log in</button>
+          </div>
+        </nav>
+
+        <div className='p-4 md:p-8'>
+          <hr className='hidden md:block mb-5'></hr>
+          <div className='grid md:grid-cols-2 gap-8'>
+            <div className='grid grid-rows-4 md:col-span-1 flex flex-col justify-between h-full'>
+              <div className='pt-14'>
+                <h1 className='text-5xl md:text-8xl'>Your first digital art collection.</h1>
+                <hr className='mt-5'></hr>
+                <div className='grid md:grid-cols-4 mt-5'>                       
+                  <div className='col-span-1 flex -space-x-2 overflow-hidden mt-5'>
+                    <Image
+                      className="inline-block h-12 w-12 rounded-full pics"
+                      src={profile1}
+                      alt=""
+                    />
+                    <Image
+                      className="inline-block h-12 w-12 rounded-full pics"
+                      src={profile3}
+                      alt=""
+                    />
+                    <Image
+                      className="inline-block h-12 w-12 rounded-full pics"
+                      src={profile2}
+                      alt=""
+                    />
                   </div>
-                  <button className='rounded-full mt-5' style={{backgroundColor: "black", color: "white", padding: 12, marginRight:12}}>GET STARTED</button>
+                  <div className='col-span-3'>
+                    <h2 className='mt-5'>JOIN OUR 150K+ COMMUNITY OF TALENTED ARTISTS</h2>
+                  </div>
                 </div>
-                <div className='flex flex-row justify-between' style={{backgroundColor: "orange", padding: 24}}>
-                  <div>
-                    <Image src={headerPic} width={100} height={100} alt='' style={{objectFit: "cover", borderRadius: 10}}></Image>
-                  </div>
-                  <div>
-                    <h2 style={{marginBottom: 12}}>We are the comprehensive online resource for digital artists that offers tutorials, tips, and tools. </h2>
-                    <button className='rounded-full' style={{backgroundColor: "black", color: "white", padding: 12, marginRight:12}}>Facebook</button>
-                    <button className='rounded-full' style={{backgroundColor: "black", color: "white", padding: 12, marginRight:12}}>Twitter</button>
-                    <button className='rounded-full' style={{backgroundColor: "black", color: "white", padding: 12, marginRight:12}}>Instagram</button>
-                  </div>
+                <button className='rounded-full mt-5 butt'>GET STARTED</button>
+              </div>
+              <div className='flex flex-row items-center social-box'>
+                <div>
+                  <Image className='pics'src={headerPic} width={200} height={200} alt='' style={{borderRadius: 5}}></Image>
+                </div>
+                <div className='ml-5'>
+                  <h2 style={{marginBottom: 12}}>We are the comprehensive online resource for digital artists that offers tutorials, tips, and tools.</h2>
+                  <button className='rounded-full butt'>Facebook</button>
+                  <button className='rounded-full butt'>Twitter</button>
+                  <button className='rounded-full butt'>Instagram</button>
                 </div>
               </div>
-
             </div>
             <div className='col-span-1'>
-              <Image src={headerPic} alt="image" style={{borderRadius: 10}}
+              <Image className='pics hidden md:block' src={headerPic} alt="image" style={{borderRadius: 10}}
               />
-
             </div>
-
           </div>
-          <hr className='mt-5' style={{borderColor: "black"}}></hr>
-
-
+          <hr className='mt-5'></hr>
         </div>
-
       </section>
 
-      <section>
-
-      </section>
-
-      <section>
-        
-      </section>
-
-      <section>
-        
-      </section>
-
-      <section>
-        
-      </section>
-
-
-      
     </main>
   )
 }
