9ada74172794dd53c8587d1d36b2bec1bee05962
daja-w <104468175+daja-w@users.noreply.github.com>
2023-11-08 22:35:03 -0700

    footer styling and copy

--- of-age-app/components/Footer.tsx
+++ of-age-app/components/Footer.tsx
@@ -9,11 +9,11 @@ function Footer(){
         <footer className='mt-32 lg:mt-48 px-2 pb-2'>
         <div className='rounded-[20px] bg-blue-200/50 py-12 px-8 lg:px-16'>
           <div className='mb-24 text-center md:text-left oswald'>
-            <h1 className='text-xl md:text-3xl text-primary'>Leading the next era of work.</h1>
+            <h1 className='text-xl md:text-3xl text-primary'>Connecting the next generation of tech.</h1>
           </div>
           <div className='mb-24 grid grid-rows-12 lg:grid-cols-3 lg:grid-rows-3 lg:gap-x-8 text-primary'>
             <a href="mailto:oafounders@gmail.com" className='animated-link flex flex-row justify-between lg:self-end'>
-              <div className='flex flex-row space-x-4'>
+              <div className='flex flex-row space-x-4 montserrat'>
                 <Mail size={28} color="#1b2954"/>
                 <p>Email Us</p>
               </div>
@@ -23,7 +23,7 @@ function Footer(){
             </a>
             <hr className="lg:hidden border-solid text-primary"></hr>
             <a href="https://www.instagram.com/oaforstudents/" className='animated-link flex flex-row justify-between lg:self-end'>
-              <div className='flex flex-row space-x-4'>
+              <div className='flex flex-row space-x-4 montserrat'>
               <Instagram size={28} color="#1b2954"/>
                 <p>Instagram</p>
               </div>
@@ -34,7 +34,7 @@ function Footer(){
             </a>
             <hr className="lg:hidden border-solid text-primary"></hr>
             <a href="https://www.linkedin.com/company/of-age/" className='animated-link flex flex-row justify-between lg:self-end'>
-              <div className='flex flex-row space-x-4'>
+              <div className='flex flex-row space-x-4 montserrat'>
               <Linkedin size={28} color="#1b2954"/>
                 <p>LinkedIn</p>
               </div>
@@ -45,7 +45,7 @@ function Footer(){
             </a>
             <hr className="lg:hidden border-solid text-primary"></hr>
             <a href="https://www.tiktok.com/" className='animated-link flex flex-row justify-between'>
-              <div className='flex flex-row space-x-4'>
+              <div className='flex flex-row space-x-4 montserrat'>
               <Music2 size={28} color="#1b2954"/>
                 <p>TikTok</p>
               </div>
@@ -56,7 +56,7 @@ function Footer(){
             </a>
             <hr className="lg:hidden border-solid text-primary"></hr>
             <a href="https://twitter.com/oaforstudents" className='animated-link flex flex-row justify-between'>
-              <div className='flex flex-row space-x-4'>
+              <div className='flex flex-row space-x-4 montserrat'>
               <Twitter size={28} color="#1b2954"/>
                 <p>Twitter</p>
               </div>
@@ -67,7 +67,7 @@ function Footer(){
             </a>
             <hr className="lg:hidden border-solid text-primary"></hr>
             <a href="https://youtube.com" className='animated-link flex flex-row justify-between'>
-              <div className='flex flex-row space-x-4'>
+              <div className='flex flex-row space-x-4 montserrat'>
               <Youtube size={28} color="#1b2954"/>
                 <p>Youtube</p>
               </div>
@@ -101,8 +101,8 @@ function Footer(){
             <a href="/" className='pr-4'>
               <img src="logo.png" className='w-[75px]'/>
             </a>
-            <div className='pl-4 text-primary'>
-              <p>We're glad you're part of this community.</p>
+            <div className='pl-4 text-primary montserrat'>
+              <p>You are the next you.</p>
             </div>
 
           </div>
