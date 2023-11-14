5d0526ed5c4360c966b6edaaeb9a878aefaea757
daja-w <104468175+daja-w@users.noreply.github.com>
2023-11-14 16:12:17 -0700

    responsive changes to early access button

--- of-age-app/components/NavBar1.tsx
+++ of-age-app/components/NavBar1.tsx
@@ -12,17 +12,17 @@ function NavBar(){
         <div className="oa-nav w-full">
             <nav className="flex justify-between items-center py-8 lg:py-4 px-11">
                 <div className="flex items-center ">
-                    <Link href="index.html" aria-current="page" className="mr-32"><Image src="/oa-word.png" alt="" width={100} height={33} /></Link>
+                    <Link href="index.html" aria-current="page" className="md:mr-32"><Image src="/oa-word.png" alt="" width={100} height={33} /></Link>
                     <div className="items-center relative h-full hidden lg:flex">
                         <Link href="/about-us" className="mr-8">
                             <div className="uppercase tracking-tight text-xs underline decoration-2 underline-offset-4">About Us</div>
                         </Link>
                     </div>
                 </div>
-                <div className=" flex flex-row items-center nav-butt signup">
+                <div className=" flex flex-row items-center rounded-[8px] md:px-7 md:py-3 px-4 py-3 signup">
                     <SignedOut>
                         <SignUpButton mode="modal">
-                        <div className="flex uppercase text-white tracking-tight text-xs cursor-pointer items-center ">Early Access 
+                        <div className="md:flex uppercase text-white tracking-tight text-xs cursor-pointer items-center ">Early Access 
                             <span className="hidden md:inline-flex items-center pl-2">
                                 <ArrowRight className="" color="white" size={20} />
                             </span>
