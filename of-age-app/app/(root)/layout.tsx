8eb47eed495e1ad6424f665790c24dec150bfde7
daja-w <104468175+daja-w@users.noreply.github.com>
2023-11-28 14:16:22 -0700

    set GA env variable

--- of-age-app/app/(root)/layout.tsx
+++ of-age-app/app/(root)/layout.tsx
@@ -19,14 +19,14 @@ export default function RootLayout({
   return (
     <ClerkProvider>
             {/* Google tag (gtag.js) */}
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-FQXEB0FYX5"/>
+            <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
                 <Script id="google-analytics" strategy="afterInteractive">
                     {`
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
 
-                        gtag('config', 'G-FQXEB0FYX5');
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
                     `}
                 </Script>
         <html lang="en">
