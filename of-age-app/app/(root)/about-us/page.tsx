35a9bd240c5d70476638af55316e86d693b362fe
daja-w <104468175+daja-w@users.noreply.github.com>
2024-02-01 15:35:05 -0700

    edit about page

--- of-age-app/app/(root)/about-us/page.tsx
+++ of-age-app/app/(root)/about-us/page.tsx
@@ -1,29 +1,28 @@
 import Image from "next/image";
 import Footer from "../../../components/Footer";
+import NavBar from "@/components/NavBar1";
 
 export default function About(){
     return(
         <main className='text-black overflow-hidden'>
-            <section className=" bg-black lg:px-[5vw] px-[6vw] pb-16">
-                <div className="text-white">
-                    <h1 className="uppercase text-6xl font-extrabold lg:pt-24 pt-16">Our story.</h1>
-                    <div className="lg:mt-[10.5vw] mt-[21vw] text-[7vw] lg:text-[3vw] leading-tight">
-                        <p className="">Capital Group is a management consulting firm with more than a decade of experience in providing effective and innovative consulting services to public and private sector clients. Our projects have saved clients over $3 billion since our inception.<br/><br/>Each day we earn the trust of our clients through solving complex program management and systems engineering challenges, while carefully managing risks. Our capabilities are rooted in experienced program management, strong technical competencies, and results that matter for our clients.
-                        </p>
+            <NavBar></NavBar>
+            <div className="px-2 mt-2">
+            <div className="bg-[--bgA1] rounded-[20px] pb-16">
+                <div className="px-8 lg:px-16 pb-10">
+                    <div className="text-white">
+                        <h1 className="uppercase text-6xl lg:pt-24 pt-16 montserrat bold-text ">Our story.</h1>
+                        <div className="mt-16 text-3xl lg:text-5xl/tight oswald">
+                            <p className="">As an ed-tech company uniquely designed for students rather than institutions, Of Age is here to enhance your university journey. Often, universities offer numerous resources for launching startups or tech ventures, but many students only discover these opportunities post-graduation. Our mission? To help you uncover and utilize the resources available at your current or future universities.<br/><br/>Why do we do this? We&apos;re students too, and we&apos;ve noticed that traditional degree and course structures don&apos;t always align with the pace of learning and collaboration needed in today&apos;s world - a world you're either about to enter or are already shaping. To kickstart our mission, we&apos;re focusing on spotlighting & connecting students who are actively utilizing school resources. Next on our agenda is addressing a critical question: how can we replicate the seamless collaboration found between engineering, design, and business sectors in the professional world within academic environments? This challenge forms the core of our roadmap and guides our efforts in reshaping educational experiences.<br/><br/>In summary, our philosophy & motivation is simple: you&apos;re already &apos;Of Age&apos; to start making an impact. Why wait? We aim to empower educators to adopt more collaborative, relevant, and engaging teaching methods. Imagine learning in an environment that mirrors the teamwork and innovation seen in top companies like Google. That&apos;s the future we&apos;re building towards with Of Age.
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="lg:mt-[10.5vw] relative mt-[11vw] lg:flex lg:flex-row lg:items-start lg:justify-items-center text-white">
-                    <div className="lg:w-[41vw] w-full">
-                        <h2 className="lg:text-[3vw] text-[7vw] leading-tight">We&apos;re not your average consulting firm.</h2>
-                        <p className="mt-[6.4vw] lg:mt-[2vw] text-[5vw] lg:text-[1.3vw]">Since 2011, Capital Group has assisted clients in achieving their business objectives. Our team&apos;s diverse background and extensive experience in both the public and private sector brings our ingenuity and pragmatism together to deliver actionable strategies for clients.</p>
-                    </div>
-                    <div className=" lg:mt-0 mt-[17vw]">
-                        <Image src="/aboutus.png" alt='' width={960} height={640} className="rounded-lg relative" style={{objectFit: "contain"}} />
-                    </div>
-                </div>
-            </section>
+            </div>
+            </div>
             
-            <div className="mt-16 lg:mt-32"> <Footer></Footer></div>
+            <div className="mt-2">
+                <Footer></Footer>
+            </div>
             
         </main>
         
