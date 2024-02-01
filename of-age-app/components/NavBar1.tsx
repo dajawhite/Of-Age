79bc2479888af41e2ead6ee7dc712608e8ef9606
daja-w <104468175+daja-w@users.noreply.github.com>
2024-02-01 13:59:40 -0700

    nav bar style changes

--- of-age-app/components/NavBar1.tsx
+++ of-age-app/components/NavBar1.tsx
@@ -15,14 +15,14 @@ function NavBar(){
                     <Link href="index.html" aria-current="page" className="md:mr-32"><Image src="/oa-word.png" alt="" width={100} height={33} /></Link>
                     <div className="items-center relative h-full hidden lg:flex">
                         <Link href="/about-us" className="mr-8">
-                            <div className="uppercase tracking-tight text-xs underline decoration-2 underline-offset-4">About Us</div>
+                            <div className="uppercase text-sm underline decoration-2 underline-offset-4">About Us</div>
                         </Link>
                     </div>
                 </div>
                 <div className=" flex flex-row items-center rounded-[8px] md:px-7 md:py-3 px-4 py-3 signup">
                     <SignedOut>
                         <SignUpButton mode="modal">
-                        <div className="md:flex uppercase text-white tracking-tight text-xs cursor-pointer items-center ">Early Access 
+                        <div className="md:flex uppercase text-white text-sm cursor-pointer items-center ">Early Access 
                             <span className="hidden md:inline-flex items-center pl-2">
                                 <ArrowRight className="" color="white" size={20} />
                             </span>
@@ -33,14 +33,7 @@ function NavBar(){
                         <div className="hidden md:flex mr-4">
                             <h1 className="font-semibold uppercase text-white">hi, {user?.firstName}</h1>
                         </div>
-                    </SignedIn>
-                    <SignedIn>
                         <UserButton afterSignOutUrl="/" />
-                        {/*<div className="uppercase text-white tracking-tight text-xs cursor-pointer">Sign Out 
-                            <span className="hidden md:inline ml-2">
-                                <Image className="" src="/white-arrow.png" alt="" width={15} height={15} />
-                            </span>
-                        </div>*/}
                     </SignedIn>
 
                 </div>
